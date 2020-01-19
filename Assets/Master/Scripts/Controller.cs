fa54af4a01a9582d06157e9a88a1883fd596432d
Chris Low <chrislow360@gmail.com>
2020-01-19 12:46:04 -0500

    intial project commit

---
+++ Assets/Master/Scripts/Controller.cs
@@ -0,0 +1,87 @@
+﻿namespace NRKernal.NRExamples
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Controller : MonoBehaviour
+    {
+        // A prefab for visualizing an TrackingImage.
+        public Visualizer flower1;
+        public Visualizer plane2;
+
+        // The overlay containing the fit to scan user guide.
+        public GameObject FitToScanOverlay;
+
+        private Dictionary<int, Visualizer> m_Visualizers = new Dictionary<int, Visualizer>();
+
+        private List<NRTrackableImage> m_TempTrackingImages = new List<NRTrackableImage>();
+
+        // Update is called once per frame
+        void Update()
+        {
+#if !UNITY_EDITOR
+            // Check that motion tracking is tracking.
+            if (NRFrame.SessionStatus != SessionState.Tracking)
+            {
+                return;
+            }
+#endif
+            // Get updated augmented images for this frame.
+            NRFrame.GetTrackables<NRTrackableImage>(m_TempTrackingImages, NRTrackableQueryFilter.New);
+
+            // Create visualizers and anchors for updated augmented images that are tracking and do not previously
+            // have a visualizer. Remove visualizers for stopped images.
+            foreach (var image in m_TempTrackingImages)
+            {
+                Visualizer visualizer = null;
+                switch (image.GetDataBaseIndex()) {
+                    case 0:
+                        m_Visualizers.TryGetValue(image.GetDataBaseIndex(), out visualizer);
+                        if (image.GetTrackingState() == TrackingState.Tracking && visualizer == null)
+                        {
+                            // based on which image is detected, display correct prefab
+                            // Create an anchor to ensure that NRSDK keeps tracking this augmented image.
+                            // Is img tracking persistant? will prefab vanish if cannot see book
+                            visualizer = (Visualizer)Instantiate(flower1, image.GetCenterPose().position, image.GetCenterPose().rotation);
+                            visualizer.pageFlower = image;
+                            visualizer.transform.parent = transform;
+                            m_Visualizers.Add(image.GetDataBaseIndex(), visualizer);
+
+                            NRDebugger.Log("Found flower1.jpg!");
+
+                        }
+                        else if (image.GetTrackingState() == TrackingState.Stopped && visualizer != null)
+                        {
+                            m_Visualizers.Remove(image.GetDataBaseIndex());
+                            Destroy(visualizer.gameObject);
+                        }
+                        break;
+                    case 1:
+                        m_Visualizers.TryGetValue(image.GetDataBaseIndex(), out visualizer);
+                        if (image.GetTrackingState() == TrackingState.Tracking && visualizer == null)
+                        {
+                            // based on which image is detected, display correct prefab
+                            // Create an anchor to ensure that NRSDK keeps tracking this augmented image.
+                            // Is img tracking persistant? will prefab vanish if cannot see book
+                            visualizer = (Visualizer)Instantiate(plane2, image.GetCenterPose().position, image.GetCenterPose().rotation);
+                            visualizer.pageFlower = image;
+                            visualizer.transform.parent = transform;
+                            m_Visualizers.Add(image.GetDataBaseIndex(), visualizer);
+
+                            NRDebugger.Log("Found plane2.jpg!");
+
+                        }
+                        else if (image.GetTrackingState() == TrackingState.Stopped && visualizer != null)
+                        {
+                            m_Visualizers.Remove(image.GetDataBaseIndex());
+                            Destroy(visualizer.gameObject);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                FitToScanOverlay.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Master/Scripts/Visualizer.cs
@@ -0,0 +1,32 @@
+﻿using NRKernal;
+using UnityEngine;
+using System.Collections;
+
+public class Visualizer : MonoBehaviour
+{
+    // The TrackingImage to visualize.
+    public NRTrackableImage pageFlower;
+
+    // A model for the page to place when an image is detected.
+    public GameObject sceneFlower;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if image tracker not found
+        if (pageFlower == null || pageFlower.GetTrackingState() != TrackingState.Tracking)
+        {
+            // do not display scene
+            sceneFlower.SetActive(false);
+            return;
+        }
+
+        // if found, set position
+        var center = pageFlower.GetCenterPose();
+        transform.position = center.position;
+        transform.rotation = center.rotation;
+
+        // display scene
+        sceneFlower.SetActive(true);
+    }
+}
